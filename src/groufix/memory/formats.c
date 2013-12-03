a778533118694af91afca6438f180a0eb50481c6
Stef Velzel <stefvelzel@hotmail.com>
2013-12-03 01:19:50 +0100

    Un-inlined some functions

--- src/groufix/memory/formats.c
+++ src/groufix/memory/formats.c
@@ -38,6 +38,69 @@
 #endif
 
 /******************************************************/
+int _gfx_is_data_type_packed(GFXDataType type)
+{
+	switch(type.packed)
+	{
+		/* Check for any packed type */
+		case GFX_UNSIGNED_SHORT_5_6_5 :
+		case GFX_UNSIGNED_SHORT_4_4_4_4 :
+		case GFX_UNSIGNED_SHORT_5_5_5_1 :
+		case GFX_INT_10_10_10_2 :
+		case GFX_UNSIGNED_INT_10_10_10_2 :
+		case GFX_UNSIGNED_INT_11F_11F_10F :
+		case GFX_UNSIGNED_INT_9_9_9_5E :
+		case GFX_UNSIGNED_INT_24_8 :
+		case GFX_FLOAT_UNSIGNED_INT_24_8 :
+			return 1;
+	}
+	return 0;
+}
+
+/******************************************************/
+unsigned char _gfx_sizeof_data_type(GFXDataType type)
+{
+	/* Check for unpacked datatypes */
+	switch(type.unpacked)
+	{
+		case GFX_BYTE :
+		case GFX_UNSIGNED_BYTE :
+			return 1;
+
+		case GFX_SHORT :
+		case GFX_UNSIGNED_SHORT :
+		case GFX_HALF_FLOAT :
+			return 2;
+
+		case GFX_INT :
+		case GFX_UNSIGNED_INT:
+		case GFX_FLOAT :
+			return 4;
+	}
+
+	/* Check for packed datatypes */
+	switch(type.packed)
+	{
+		case GFX_UNSIGNED_SHORT_5_6_5 :
+		case GFX_UNSIGNED_SHORT_4_4_4_4 :
+		case GFX_UNSIGNED_SHORT_5_5_5_1 :
+			return 2;
+
+		case GFX_INT_10_10_10_2 :
+		case GFX_UNSIGNED_INT_10_10_10_2 :
+		case GFX_UNSIGNED_INT_11F_11F_10F :
+		case GFX_UNSIGNED_INT_9_9_9_5E :
+		case GFX_UNSIGNED_INT_24_8 :
+			return 4;
+
+		case GFX_FLOAT_UNSIGNED_INT_24_8 :
+			return 8;
+	}
+
+	return 0;
+}
+
+/******************************************************/
 GLint _gfx_texture_format_to_pixel_format(GFXTextureFormat format)
 {
 	/* Check for packed formats */
--- src/groufix/memory/internal.h
+++ src/groufix/memory/internal.h
@@ -104,7 +104,24 @@ void _gfx_program_force_use(GLuint handle, const GFX_Extensions* ext);
 
 
 /********************************************************
- * Format retrieval (of textures)
+ * Datatype helpers
+ *******************************************************/
+
+/**
+ * Returns 1 if packed data, 0 if unpacked.
+ *
+ */
+int _gfx_is_data_type_packed(GFXDataType type);
+
+/**
+ * Returns the size of a data type in bytes.
+ *
+ */
+unsigned char _gfx_sizeof_data_type(GFXDataType type);
+
+
+/********************************************************
+ * Format retrieval (of textures) & converters
  *******************************************************/
 
 /**
@@ -118,11 +135,6 @@ GLint _gfx_texture_get_internal_format(const GFXTexture* texture);
  *
  */
 GLenum _gfx_texture_get_internal_target(const GFXTexture* texture);
-
-
-/********************************************************
- * Format converters
- *******************************************************/
 
 /**
  * Converts a texture format to a client pixel format.
@@ -145,80 +157,6 @@ GLint _gfx_texture_format_to_internal(GFXTextureFormat format);
  *
  */
 GFXTextureFormat _gfx_texture_format_from_internal(GLint format);
-
-
-/********************************************************
- * Datatype helpers
- *******************************************************/
-
-/**
- * Returns 1 if packed data, 0 if unpacked.
- *
- */
-inline int _gfx_is_data_type_packed(GFXDataType type)
-{
-	switch(type.packed)
-	{
-		/* Check for any packed type */
-		case GFX_UNSIGNED_SHORT_5_6_5 :
-		case GFX_UNSIGNED_SHORT_4_4_4_4 :
-		case GFX_UNSIGNED_SHORT_5_5_5_1 :
-		case GFX_INT_10_10_10_2 :
-		case GFX_UNSIGNED_INT_10_10_10_2 :
-		case GFX_UNSIGNED_INT_11F_11F_10F :
-		case GFX_UNSIGNED_INT_9_9_9_5E :
-		case GFX_UNSIGNED_INT_24_8 :
-		case GFX_FLOAT_UNSIGNED_INT_24_8 :
-			return 1;
-	}
-	return 0;
-}
-
-/**
- * Returns the size of a data type in bytes.
- *
- */
-inline unsigned char _gfx_sizeof_data_type(GFXDataType type)
-{
-	/* Check for unpacked datatypes */
-	switch(type.unpacked)
-	{
-		case GFX_BYTE :
-		case GFX_UNSIGNED_BYTE :
-			return 1;
-
-		case GFX_SHORT :
-		case GFX_UNSIGNED_SHORT :
-		case GFX_HALF_FLOAT :
-			return 2;
-
-		case GFX_INT :
-		case GFX_UNSIGNED_INT:
-		case GFX_FLOAT :
-			return 4;
-	}
-
-	/* Check for packed datatypes */
-	switch(type.packed)
-	{
-		case GFX_UNSIGNED_SHORT_5_6_5 :
-		case GFX_UNSIGNED_SHORT_4_4_4_4 :
-		case GFX_UNSIGNED_SHORT_5_5_5_1 :
-			return 2;
-
-		case GFX_INT_10_10_10_2 :
-		case GFX_UNSIGNED_INT_10_10_10_2 :
-		case GFX_UNSIGNED_INT_11F_11F_10F :
-		case GFX_UNSIGNED_INT_9_9_9_5E :
-		case GFX_UNSIGNED_INT_24_8 :
-			return 4;
-
-		case GFX_FLOAT_UNSIGNED_INT_24_8 :
-			return 8;
-	}
-
-	return 0;
-}
 
 
 #ifdef __cplusplus
