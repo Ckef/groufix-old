7eddd6a408872181b29aacbe599e8579872010a2
Stef Velzel <stefvelzel@hotmail.com>
2015-04-06 23:06:34 +0200

    Batch interface

--- include/groufix/scene/object.h
+++ include/groufix/scene/object.h
@@ -34,11 +34,11 @@ typedef struct GFXBatch
 
 	GFXMaterial*   material;
 	GFXMesh*       mesh;
-	unsigned int   materialIndex; /* Index into the material level */
-	unsigned int   meshIndex;     /* Index into the mesh level */
+	unsigned int   materialIndex; /* Index into all material levels */
+	unsigned int   meshIndex;     /* Index into all mesh levels */
 
-	unsigned int   copies;        /* Number of copies to reserve */
-	unsigned char  levels;        /* Number of mesh/material level combinations */
+	unsigned char  levels;        /* Number of mesh/material LOD combinations */
+	unsigned int   units;         /* Number of units to reserve per level */
 
 } GFXBatch;
 
@@ -49,20 +49,20 @@ typedef struct GFXBatch
  * @param bucket    Bucket all units are insert into.
  * @param matIndex  Index into all material levels.
  * @param meshIndex Index into all mesh levels.
- * @param copies    Number of copies (ergo units) to reserve per level.
- * @param levels    Number of mesh/material level combinations.
+ * @param levels    Number of mesh/material LOD combinations.
+ * @param units     Number of units to reserve per level.
  * @return NULL on failure.
  *
  */
 GFX_API GFXBatch* gfx_batch_create(
 
-		GFXBucket*    bucket,
-		GFXMaterial*  material,
-		GFXMesh*      mesh,
-		unsigned int  matIndex,
-		unsigned int  meshIndex,
-		unsigned int  copies,
-		unsigned int  levels);
+		GFXBucket*     bucket,
+		GFXMaterial*   material,
+		GFXMesh*       mesh,
+		unsigned int   matIndex,
+		unsigned int   meshIndex,
+		unsigned char  levels,
+		unsigned int   units);
 
 /**
  * Makes sure the batch is freed properly.
@@ -71,6 +71,90 @@ GFX_API GFXBatch* gfx_batch_create(
 GFX_API void gfx_batch_free(
 
 		GFXBatch* batch);
+
+/**
+ * Sets the properties of a level at a batch.
+ *
+ * @param level    Level to set properties of (must be < batch->levels).
+ * @param material LOD of the material to use.
+ * @param mesh     LOD of the mesh to use.
+ * @param copies   Number of copies to reserve at the property map (must be >= 1).
+ * @return Zero on failure.
+ *
+ * Note: this will reallocate copies at the property map, which is expensive!
+ *
+ */
+GFX_API int gfx_batch_set_level(
+
+		GFXBatch*      batch,
+		unsigned char  level,
+		unsigned int   material,
+		unsigned int   mesh,
+		unsigned int   copies);
+
+/**
+ * Sets the properties of a level at a batch and shares copies.
+ *
+ * @param src      Batch to share copies with.
+ * @param srcLevel Level at the source batch to share with.
+ * @return Zero on failure.
+ *
+ * Note: this will fail if the batches do not share the material index,
+ * as it will attempt to share copies at a property map.
+ *
+ */
+GFX_API int gfx_batch_set_level_share(
+
+		GFXBatch*      batch,
+		unsigned char  level,
+		GFXBatch*      src,
+		unsigned char  srcLevel,
+		unsigned int   mesh);
+
+/**
+ * Fetches the properties of a level at a batch.
+ *
+ * @param copies    Returns the number of reserved copies.
+ * @param instances Returns maximum number of instances per unit, 0 for infinite.
+ * @param offset    Returns the starting of the reserved range.
+ * @return Zero if the properties are not set yet, no parameter is written to.
+ *
+ */
+GFX_API int gfx_batch_get_level(
+
+		GFXBatch*      batch,
+		unsigned char  level,
+		unsigned int*  copies,
+		unsigned int*  instances,
+		unsigned int*  offset);
+
+/**
+ * Sets the number of allocated units at a level at a batch.
+ *
+ * @param num Number of units to allocate.
+ * @return Array of allocated bucket units (of num length), NULL on failure.
+ *
+ * If num is less than currently allocated units, units will be destroyed.
+ *
+ */
+GFX_API GFXBucketUnit* gfx_batch_set(
+
+		GFXBatch*      batch,
+		unsigned char  level,
+		unsigned int   num);
+
+/**
+ * Fetches the allocated units at a level at a batch.
+ *
+ * @param num Returns the number of units in the returned array.
+ * @return Array of allocated bucket units.
+ *
+ */
+GFX_API GFXBucketUnit* gfx_batch_get(
+
+		const GFXBatch*  batch,
+		unsigned char    level,
+		unsigned int*    num);
 
 
 #ifdef __cplusplus
--- src/groufix/scene/batch.c
+++ src/groufix/scene/batch.c
@@ -23,9 +23,13 @@
 /* Internal level pair */
 typedef struct GFX_Level
 {
-	unsigned int offset;   /* Copy offset */
-	unsigned int material; /* Material LOD */
-	unsigned int mesh;     /* Mesh LOD */
+	unsigned int material;  /* Material LOD */
+	unsigned int mesh;      /* Mesh LOD */
+	unsigned int num;
+
+	unsigned int copies;    /* Number of reserved copies (0 if uninitialized level) */
+	unsigned int instances; /* Maximum number of instances per unit, 0 for infinite */
+	unsigned int offset;    /* Copy offset */
 
 } GFX_Level;
 
@@ -44,40 +48,31 @@ static inline GFXBucketUnit* _gfx_batch_get_unit(
 
 		GFXBatch*     batch,
 		unsigned int  level,
-		unsigned int  copy)
+		unsigned int  unit)
 {
 	return ((GFXBucketUnit*)(((GFX_Level*)(batch + 1)) + batch->levels)) +
-		(batch->levels * copy + level);
-}
-
-/******************************************************/
-static inline int _gfx_batch_has_level(
-
-		GFXBatch*     batch,
-		unsigned int  level)
-{
-	return *_gfx_batch_get_unit(batch, level, 0) != 0;
+		(batch->units * level + unit);
 }
 
 /******************************************************/
 GFXBatch* gfx_batch_create(
 
-		GFXBucket*    bucket,
-		GFXMaterial*  material,
-		GFXMesh*      mesh,
-		unsigned int  matIndex,
-		unsigned int  meshIndex,
-		unsigned int  copies,
-		unsigned int  levels)
+		GFXBucket*     bucket,
+		GFXMaterial*   material,
+		GFXMesh*       mesh,
+		unsigned int   matIndex,
+		unsigned int   meshIndex,
+		unsigned char  levels,
+		unsigned int   units)
 {
 	/* ffs.. */
-	if(!copies || !levels) return NULL;
+	if(!levels || !units) return NULL;
 
 	/* Create new batch, append level pairs and units to the end of struct */
 	size_t size =
 		sizeof(GFXBatch) +
 		sizeof(GFX_Level) * levels +
-		sizeof(GFXBucketUnit) * levels * copies;
+		sizeof(GFXBucketUnit) * levels * units;
 
 	GFXBatch* batch = malloc(size);
 	if(!batch)
@@ -103,15 +98,11 @@ GFXBatch* gfx_batch_create(
 	batch->mesh          = mesh;
 	batch->materialIndex = matIndex;
 	batch->meshIndex     = meshIndex;
-	batch->copies        = copies;
 	batch->levels        = levels;
+	batch->units         = units;
 
-	/* Set first units of all levels to 0 to indicate unallocated */
-	memset(
-		_gfx_batch_get_unit(batch, 0, 0),
-		0,
-		levels * sizeof(GFXBucketUnit)
-	);
+	/* Initialize levels to 0 to indicate unallocated */
+	memset(batch + 1, 0, sizeof(GFX_Level) * levels);
 
 	return batch;
 }
@@ -128,4 +119,60 @@ void gfx_batch_free(
 
 		free(batch);
 	}
+}
+
+/******************************************************/
+int gfx_batch_set_level(
+
+		GFXBatch*      batch,
+		unsigned char  level,
+		unsigned int   material,
+		unsigned int   mesh,
+		unsigned int   copies)
+{
+	return 0;
+}
+
+/******************************************************/
+int gfx_batch_set_level_share(
+
+		GFXBatch*      batch,
+		unsigned char  level,
+		GFXBatch*      src,
+		unsigned char  srcLevel,
+		unsigned int   mesh)
+{
+	return 0;
+}
+
+/******************************************************/
+int gfx_batch_get_level(
+
+		GFXBatch*      batch,
+		unsigned char  level,
+		unsigned int*  copies,
+		unsigned int*  instances,
+		unsigned int*  offset)
+{
+	return 0;
+}
+
+/******************************************************/
+GFXBucketUnit* gfx_batch_set(
+
+		GFXBatch*      batch,
+		unsigned char  level,
+		unsigned int   num)
+{
+	return NULL;
+}
+
+/******************************************************/
+GFXBucketUnit* gfx_batch_get(
+
+		const GFXBatch*  batch,
+		unsigned char    level,
+		unsigned int*    num)
+{
+	return NULL;
 }
