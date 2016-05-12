1f7d2efa5ef3320ec800527b25448d288e4f1df2
Stef Velzel <stefvelzel@hotmail.com>
2016-05-12 20:51:37 +0200

    Fixed some win32 issues regarding bit depth

--- src/groufix/core/platform/win32_monitor.c
+++ src/groufix/core/platform/win32_monitor.c
@@ -77,12 +77,12 @@ int _gfx_platform_monitor_get_mode(
 	/* Split depth */
 	_gfx_split_depth(
 		dev->dmBitsPerPel,
-		&mode->depth[0],
-		&mode->depth[1],
-		&mode->depth[2]
+		&mode->depth.data[0],
+		&mode->depth.data[1],
+		&mode->depth.data[2]
 	);
 
-	mode->depth[3] = 0;
+	mode->depth.data[3] = 0;
 
 	return 1;
 }
--- src/groufix/core/platform/win32_window.c
+++ src/groufix/core/platform/win32_window.c
@@ -13,6 +13,7 @@
  */
 
 #include "groufix/core/platform/win32.h"
+#include "groufix/core/utils.h"
 
 #include <stdlib.h>
 
